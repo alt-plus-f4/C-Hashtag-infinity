078b59ae5d755af123eea918062eb80c209e3f1c
SashoJoto <sasho2468@gmail.com>
2022-03-12 15:49:25 +0200

    Redirect page fixed

--- static/js/redirecting.js
+++ static/js/redirecting.js
@@ -2,7 +2,7 @@ let redirect = () => {
     const btn = document.querySelector('.search__submit');
     const input = document.querySelector('input');
     btn.addEventListener('click', () => {
-        location.href.replace('/news', '');
         location.href += `../articles?search=${input.value}`;
     });
 }
+
