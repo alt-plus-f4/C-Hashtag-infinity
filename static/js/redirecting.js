0cad5245359500fc256bb1a77f8dcda29c31dc13
SashoJoto <sasho2468@gmail.com>
2022-03-12 20:35:34 +0200

    add latest section and fixed redirecting

--- static/js/redirecting.js
+++ static/js/redirecting.js
@@ -1,8 +1,14 @@
 let redirect = () => {
     const btn = document.querySelector('.search__submit');
     const input = document.querySelector('input');
-    btn.addEventListener('click', () => {
-        location.href += `../articles?search=${input.value}`;
-    });
+    location.href += `../articles?search=${input.value}`;
 }
+
+const input = document.querySelector('input');
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        location.href += `../articles?search=${input.value}`;
+    }
+});
+
 
