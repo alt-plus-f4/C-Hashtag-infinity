0ce87a27f96b03deee5c2ce0a2fc65d4ed76d211
SashoJoto <sasho2468@gmail.com>
2022-03-12 20:43:30 +0200

    fixed redirecting

--- static/js/redirecting.js
+++ static/js/redirecting.js
@@ -1,15 +1,8 @@
 let redirect = () => {
     const btn = document.querySelector('.search__submit');
     const input = document.querySelector('input');
-<<<<<<< HEAD
     location.href += `../articles?search=${input.value}`;
-=======
-    btn.addEventListener('click', () => {
-        location.href += `/articles?search=${input.value}`;
-    });
->>>>>>> 34ec03411e4c50855ff992db6ea490cc5bf8149b
 }
-
 const input = document.querySelector('input');
 document.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
