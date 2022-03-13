36131a79dbf91b564fd5b9aac4ae400a4dead651
MrGoblings <emomomchev@gmail.com>
2022-03-13 07:07:42 +0200

    Working home page

--- static/js/redirecting.js
+++ static/js/redirecting.js
@@ -1,26 +1,26 @@
 let redirect = () => {
     const btn = document.querySelector('.search__submit');
     const input = document.querySelector('input');
-    location.href = "/path"
-    location.href += `/articles?search=${input.value}`;
+    //location.assign("/");
+    location.href = `/articles?search=${input.value}`;
 }
 const input = document.querySelector('input');
 document.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
-        location.href = "/path"
-        location.href += `/articles?search=${input.value}`;
+        //location.assign("/");
+        location.href = `/articles?search=${input.value}`;
     }
 });
 
 let redirectUrl = (id) => {
-    location.href = "/path"
-    location.href += `/article?id=${id}`;
+    //location.assign("/");
+    location.href = `/article?id=${id}`;
 }
 
 let redirectArticle = (id) => {
     let url = id.innerHTML;
-    location.href = "/path"
-    location.href += `/article?url=${url}`;
+    //location.assign("/");
+    location.href = `/article?url=${url}`;
 }
 
 
